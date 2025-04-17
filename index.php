58ecabac17872ef6ec4fbbdc630f9cc279f78010
Mohammad Rayhan Rizki <mohammad.rayhan3645@smk.belajar.id>
2025-04-17 08:34:20 +0700

    tes lagites!

---
+++ index.php
@@ -0,0 +1,21 @@
+<php?
+
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title></title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="">
+  </head>
+  <body>
+    <h1>Welcome to the PHP Web Application</h1>
+    <p>This is a simple PHP web application.</p>
+  </body>
+</html>
+
+
+?>
